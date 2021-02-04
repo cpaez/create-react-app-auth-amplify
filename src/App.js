2433a08e01926c34834d2e3e4b03608677b40de1
Carlos Paez <carpaez@gmail.com>
2021-02-04 19:53:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+
+  state = {
+    scores: []
+  }
+
+  componentDidMount() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "data": [10844,8127,6210] })
+    };
+    fetch('https://a0ouvefenc.execute-api.us-east-1.amazonaws.com/TEST/detectanomaly', requestOptions)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ scores: data })
+      console.log(data)
+    })
+    .catch(console.log)
+  }
+
   render() {
     return (
       <div className="App">
