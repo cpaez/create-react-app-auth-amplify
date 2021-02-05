2e6f9d236b50896865d01a8c732934789bc0028f
Carlos Paez <carpaez@gmail.com>
2021-02-05 15:14:35 -0300

    added score rendering

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import React, { Component } from 'react';
+import Players from './components/players';
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -32,16 +34,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
+          <Players players={this.state.players} />
+          </p>
+          <p>
           <iframe src="https://g-a1850b9265.grafana-workspace.us-east-1.amazonaws.com/d-solo/BXvoh8LGz/test-dashboard?orgId=1&refresh=5s&from=1612456659278&to=1612478259278&panelId=6" width="450" height="200" frameborder="0"></iframe>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
---
+++ src/components/players.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+    const Players = ({ players }) => {
+      return (
+        <div>
+          <center><h1>Players List</h1></center>
+          {contacts.map((player) => (
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Messi</h5>
+                <h6 class="card-subtitle mb-2 text-muted">10</h6>
+                <p class="card-text">{player.score}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    };
+
+    export default Players
