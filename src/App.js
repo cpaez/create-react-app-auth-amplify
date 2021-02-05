d6b2e80176fc06bae9fd4f089b70709ffd9a5156
Carlos Paez <carpaez@gmail.com>
2021-02-05 15:36:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "data": [10,1,4] })
+      body: JSON.stringify({ "data": [10] })
     };
     fetch('https://a0ouvefenc.execute-api.us-east-1.amazonaws.com/TEST/predictanomaly', requestOptions)
     .then(res => res.json())
