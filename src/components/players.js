f18fe319171256b6c7301734809b2b90da6084a0
Carlos Paez <carpaez@gmail.com>
2021-02-28 15:49:18 -0300

    Update players.js

--- src/components/players.js
+++ src/components/players.js
@@ -3,12 +3,12 @@ import React from 'react'
     const Players = ({ players }) => {
       return (
         <div>
-          <center><h1>Players List</h1></center>
+          <center><h1>Anomalies</h1></center>
           {players.map((player) => (
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Messi (10)</h5>
                 <p class="card-text">Anomaly score: {player.score}</p>
+                <div style="color: red">Detected abnormal player metrics, minute: {player.score}</div>
               </div>
             </div>
           ))}
