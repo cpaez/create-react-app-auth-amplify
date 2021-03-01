5c94902bc835b19f33e0a2984eafdd147b31c893
Carlos Paez <carpaez@gmail.com>
2021-03-01 12:10:47 -0300

    Update anomalies.js

--- src/components/anomalies.js
+++ src/components/anomalies.js
@@ -3,6 +3,7 @@ import React from 'react'
     const Anomalies = ({ anomalies }) => {
       return (
         <div class="card-text">
+          <h5 class="card-title">Buscando Insights...</h5>
           {anomalies.map((anomaly) => (
               <div class="card-text">{anomaly}</div>
           ))}
